dc42378fcf0e574ef547138521f49951302df8d1
Max Schwab <max.schwab@beronet.com>
2021-08-26 13:44:25 +0200

    added libuser + main tests

---
+++ src/oszimt/LibUser.java
@@ -0,0 +1,56 @@
+package oszimt;
+
+public class LibUser {
+
+    private int id;
+    private String name;
+    private int feeState;
+    private LibCard card;
+
+
+    public LibUser() {
+        this.id = (int)(Math.random()*((99999-10000)+1)) +1;
+        this.name = "not set";
+        this.feeState = 0;
+        this.card = new LibCard();
+    }
+
+    public LibUser(String name) {
+        this.id = (int)(Math.random()*((99999-10000)+1)) +1;
+        this.name = name;
+        this.feeState = 0;
+        this.card = new LibCard();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFeeState() {
+        return feeState;
+    }
+
+    public void setFeeState(int feeState) {
+        this.feeState = feeState;
+    }
+
+    public LibCard getCard() {
+        return card;
+    }
+
+    public void setCard(LibCard card) {
+        this.card = card;
+    }
+}
--- src/oszimt/Main.java
+++ src/oszimt/Main.java
@@ -12,32 +12,66 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* Create Object user1 of LibCard */
-        LibCard user1;
-        user1 = new LibCard();
+        /* Testing LibCard Class */
+        System.out.println("\n--------\nLibCard Class");
 
-        /* Print out user1 (LibCard Object) Vars */
-        System.out.println("user ID: " + user1.getId());
-        System.out.println("Expiration time in s: " + user1.getExpiry_date());
-        System.out.println("Valid: " + user1.isValid());
+        /* Create Object card1 of LibCard Class */
+        LibCard card1;
+        card1 = new LibCard();
+
+        System.out.println("user ID: " + card1.getId());
+        System.out.println("Expiration time in s: " + card1.getExpiry_date());
+        System.out.println("Valid: " + card1.isValid());
 
         /* change Validity to false */
         System.out.println("\n*block card*\n");
-        user1.setValid(false);
-        System.out.println("Valid: " + user1.isValid());
+        card1.setValid(false);
+        System.out.println("Valid: " + card1.isValid());
 
         /* change Validity to true */
         System.out.println("\n*unblock card*\n");
-        user1.setValid(true);
-        System.out.println("Valid: " + user1.isValid());
+        card1.setValid(true);
+        System.out.println("Valid: " + card1.isValid());
 
-        /* Print out BorrowCondition Vars*/
-        System.out.println("\n--------\nBorrowCondition");
+        /* Testing BorrowConditions Class*/
+        System.out.println("\n--------\nBorrowCondition Class");
 
         System.out.println("Borrow Time: " +            BorrowCondition.getBorrow_time());
         System.out.println("Expiry Fee: " +             BorrowCondition.getExpiry_fee());
         System.out.println("Maximum Fee: " +            BorrowCondition.getMax_fee());
         System.out.println("Maximum Renewal Time: " +   BorrowCondition.getMax_renewal());
+
+        /* Test LibUser Class */
+        System.out.println("\n--------\nLibUser Class");
+
+        /* Create object user1 of LibUser Class */
+        LibUser user1;
+        user1 = new LibUser("Max Schwab");
+
+        System.out.println("User ID: " +  user1.getId());
+        System.out.println("User Name: " + user1.getName());
+        System.out.println("Fee State: " + user1.getFeeState());
+
+        /* Print out LibCard of user */
+        System.out.println("\n--------\nLibCard");
+
+        System.out.println("User Card ID: " +  user1.getCard().getId());
+        System.out.println("User Card Expiration: " + user1.getCard().getExpiry_date());
+        System.out.println("User Card Valid: " + user1.getCard().isValid());
+
+        /* Extend Card */
+        user1.getCard().extend();
+        System.out.println("\n*Extend Card Expiration Date*\n");
+        System.out.println("User Card Expiration Extended: " + user1.getCard().getExpiry_date());
+
+        /* Lock Card */
+        user1.getCard().setValid(false);
+        System.out.println("User Card Number: " + user1.getCard().getId());
+        System.out.println("User Card Validity: " + user1.getCard().isValid());
+
+
+        /* Test Book Class */
+        //System.out.println("\n--------\nBook Class");
 
         /* Render GUI window for creating new LibCard
          * Unimportant for now */
